961086a98e451bc2198590e103c15b9d353b0c1f
Chris Thoburn <cthoburn@linkedin.com>
2018-11-28 21:01:41 -0800

    asdf

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 'use strict';
+const Funnel = require('broccoli-funnel');
+const BroccoliDebug = require('broccoli-debug');
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  init() {
+    this._super.init && this._super.init.apply(this, arguments);
+    this.debugTree = BroccoliDebug.buildDebugCallback('nrpae');
+  },
+
+  treeForAddon() {
+    let addonTree = new Funnel('.', {
+      include: [
+        'rest/*',
+        'rest.js'
+      ]
+    });
+
+    return this.debugTree(addonTree, 'custom-addon-tree');
+  }
 };
